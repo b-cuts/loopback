1db9a9490a9d759b9cd73b2dbd9dafbb64cf306e
Loay <loay@users.noreply.github.com>
2016-06-09 04:20:50 -0400

    update middleware

--- server/middleware/error-handler.js
+++ server/middleware/error-handler.js
@@ -3,15 +3,7 @@
 // This file is licensed under the MIT License.
 // License text available at https://opensource.org/licenses/MIT
 
-var strongErrorHandler = require('strong-error-handler');
-strongErrorHandler.title = 'Loopback';
-
-module.exports = errorHandler;
-
-function  errorHandler(options) {
-  if (!options || options.debug !== false) {
-    return strongErrorHandler(options);
-  } else {
-    throw new Error('expressHandler is not available. Please use strong-error-handler');
-  }
+module.exports = function(options) {
+  throw new Error ('expressHandler is no longer coupled with loopback.\
+  Please migrate to strong-error-handler');
 };
